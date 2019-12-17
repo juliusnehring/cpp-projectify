e47a95ed8824f24754c88a11e0f88087e1fba4a5
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2019-12-17 14:16:42 +0100

    Fixed library ordering Can now generate into an existing git repo

--- create_script.js
+++ create_script.js
@@ -1,7 +1,5 @@
-class Library
-{
-    constructor(name, url)
-    {
+class Library {
+    constructor(name, url) {
         this.name = name;
         this.url = url;
         this.dependencies = [];
@@ -13,21 +11,17 @@ var libraries = [];
 
 // make sure the order in which these are pushed into the array is sensible!
 
-libraries.push(new Library("imgui-lean","https://www.graphics.rwth-aachen.de:9000/ptrettner/imgui-lean.git"));
-libraries.push(new Library("ctracer","https://www.graphics.rwth-aachen.de:9000/ptrettner/ctracer.git"));
-libraries.push(new Library("polymesh","https://www.graphics.rwth-aachen.de:9000/ptrettner/polymesh.git"));
-libraries.push(new Library("glfw","https://github.com/glfw/glfw.git"));
-libraries.push(new Library("glow","https://www.graphics.rwth-aachen.de:9000/Glow/glow.git"));
-libraries.push(new Library("glow-extras","https://www.graphics.rwth-aachen.de:9000/Glow/glow-extras.git"));
-libraries.push(new Library("typed-geometry","https://www.graphics.rwth-aachen.de:9000/ptrettner/typed-geometry.git"));
+libraries.push(new Library("imgui-lean", "https://www.graphics.rwth-aachen.de:9000/ptrettner/imgui-lean.git"));
+libraries.push(new Library("ctracer", "https://www.graphics.rwth-aachen.de:9000/ptrettner/ctracer.git"));
+libraries.push(new Library("typed-geometry", "https://www.graphics.rwth-aachen.de:9000/ptrettner/typed-geometry.git"));
+libraries.push(new Library("polymesh", "https://www.graphics.rwth-aachen.de:9000/ptrettner/polymesh.git"));
+libraries.push(new Library("glfw", "https://github.com/glfw/glfw.git"));
+libraries.push(new Library("glow", "https://www.graphics.rwth-aachen.de:9000/Glow/glow.git"));
+libraries.push(new Library("glow-extras", "https://www.graphics.rwth-aachen.de:9000/Glow/glow-extras.git"));
 
-
-function add_dependency(name, dependency)
-{
-    for(const lib of libraries)
-    {
-        if(lib.name == name)
-        {   
+function add_dependency(name, dependency) {
+    for (const lib of libraries) {
+        if (lib.name == name) {
             lib.dependencies.push(dependency);
         }
     }
@@ -37,66 +31,54 @@ add_dependency("glow-extras", "glow");
 add_dependency("glow", "glfw");
 add_dependency("glow", "typed-geometry");
 
-function get_lib(name)
-{
-    for(const lib of libraries)
-    {
-        if(lib.name == name){
+function get_lib(name) {
+    for (const lib of libraries) {
+        if (lib.name == name) {
             return lib;
         }
     }
     return null;
 }
 
-function disable_lib(name)
-{
-    for(lib of libraries)
-    {
-        if(lib.dependencies.includes(name))
-        {
+function disable_lib(name) {
+    for (lib of libraries) {
+        if (lib.dependencies.includes(name)) {
             var othercb = lib.checkbox;
             othercb.checked = false;
             disable_lib(lib.name);
-        }   
-    }        
+        }
+    }
 }
 
-function enable_lib(name)
-{
+function enable_lib(name) {
     lib = get_lib(name);
-    for(const depend of lib.dependencies)
-    {
+    for (const depend of lib.dependencies) {
         var othercb = get_lib(depend).checkbox;
         othercb.checked = true;
         enable_lib(depend);
-    }    
+    }
 }
 
-function onCheckboxClicked(cb)
-{
+function onCheckboxClicked(cb) {
     var name = cb.value;
-    if(cb.checked)
-    {
-       enable_lib(name);
+    if (cb.checked) {
+        enable_lib(name);
     }
-    else
-    {
+    else {
         disable_lib(name);
     }
 }
 
-function setupCheckboxes()
-{
+function setupCheckboxes() {
     container = document.getElementById("libraries_div");
-    for(lib of libraries)
-    {
+    for (lib of libraries) {
         var checkbox = document.createElement("input");
         var cbId = lib.name + "Checkbox";
         checkbox.id = cbId;
         checkbox.type = "checkbox";
         checkbox.textContent = lib.name;
         checkbox.value = lib.name;
-        checkbox.onclick = function(){onCheckboxClicked(this);};
+        checkbox.onclick = function () { onCheckboxClicked(this); };
         var label = document.createElement("label");
         label.setAttribute("for", cbId);
         label.innerHTML = lib.name;
@@ -107,47 +89,38 @@ function setupCheckboxes()
     }
 }
 
-function download(filename, text) 
-{
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-  element.click();
+    element.click();
 
-  document.body.removeChild(element);
+    document.body.removeChild(element);
 }
 
-function onGenerateCommandClicked()
-{
+function onGenerateCommandClicked() {
     document.getElementById("generated_command").innerHTML = generate_command();
 }
 
-function depends_on(lib, dependee)
-{
+function depends_on(lib, dependee) {
     return lib.dependencies.includes(dependee.name);
 }
 
-function getEnabledLibraries()
-{
+function getEnabledLibraries() {
     var libs = new Array();
-    for(lib of libraries)
-    {
-        if(lib.checkbox && lib.checkbox.checked)
-        {
+    for (lib of libraries) {
+        if (lib.checkbox && lib.checkbox.checked) {
             libs.push(lib);
         }
     }
     // insertion sort
-    for (var i = 1; i < libs.length; i++) 
-    {
-        for(var j = i; j < libs.length; j++)
-        {
-            if(depends_on(libs[i], libs[j]))
-            {
+    for (var i = 1; i < libs.length; i++) {
+        for (var j = i; j < libs.length; j++) {
+            if (depends_on(libs[i], libs[j])) {
                 var tmp = libs[i];
                 libs[i] = libs[j];
                 libs[j] = tmp;
@@ -161,56 +134,43 @@ const copyToClipboard = str => {
     const el = document.createElement('textarea');  // Create a <textarea> element
     el.value = str;                                 // Set its value to the string that you want copied
     el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
-    el.style.position = 'absolute';                 
+    el.style.position = 'absolute';
     el.style.left = '-9999px';                      // Move outside the screen to make it invisible
     document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
-    const selected =            
-      document.getSelection().rangeCount > 0        // Check if there is any content selected previously
-        ? document.getSelection().getRangeAt(0)     // Store selection if found
-        : false;                                    // Mark as false to know no selection existed before
+    const selected =
+        document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+            ? document.getSelection().getRangeAt(0)     // Store selection if found
+            : false;                                    // Mark as false to know no selection existed before
     el.select();                                    // Select the <textarea> content
     document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
     document.body.removeChild(el);                  // Remove the <textarea> element
     if (selected) {                                 // If a selection existed before copying
-      document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
-      document.getSelection().addRange(selected);   // Restore the original selection
+        document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+        document.getSelection().addRange(selected);   // Restore the original selection
     }
-  };
+};
 
-function checkValidNameAndFolder()
-{
-    var projectName = document.getElementById("project_name_text_field").value;
-    var projectFolder = document.getElementById("folder_name_text_field").value;
+function checkValidGitUrl(url) {
+    // extremely simple test for now:
+    return (url.startswith("git://") || url.startswith("git@") || url.startswith("https://")) && url.endswith(".git");
+}
 
-    if(!projectName)
-    {
+function checkValidName(name) {
+    if (!name) {
         window.alert("Project name is empty!");
-        return false;        
-    }
-
-    if(!projectFolder)
-    {
-        window.alert("Project folder is empty!");
-        return false;        
-    }
-
-    if(projectName.split(" ").length != 1)
-    {
-        window.alert("Project name must be a single word!");
         return false;
     }
-    if(projectFolder.split(" ").length != 1){
-        window.alert("Project folder must be a single word!");
+
+    if (name.split(" ").length != 1) {
+        window.alert("Project name must be a single word!");
         return false;
     }
 
     return true;
 }
 
-function onGenerateButtonClick()
-{
-    if(checkValidNameAndFolder())
-    {
+function onGenerateButtonClick() {
+    if (checkValidName(document.getElementById("project_name_text_field").value)) {
         var script = generate_script();
         var output = document.getElementById("output_textarea");
         output.innerHTML = script;
@@ -219,49 +179,69 @@ function onGenerateButtonClick()
 }
 
 function decodeHtml(html) {
-    var txt = document.createElement("textarea");
+    var txt = document.createElement("textarea"); ``
     txt.innerHTML = html;
     return txt.value;
 }
 
-function onCopyToClipboardClick()
-{
+function onCopyToClipboardClick() {
     var output = document.getElementById("output_textarea");
     copyToClipboard(decodeHtml(output.innerHTML));
 }
 
-function contains(libraries, name)
-{
-    for(const lib of libraries){
-        if(lib.name == name)
+function contains(libraries, name) {
+    for (const lib of libraries) {
+        if (lib.name == name)
             return true;
     }
     return false;
 }
 
-function generate_script()
-{
+function nameFromGitUrl(url) {
+    if (!checkValidGitUrl(url)) {
+        return null;
+    }
+    var name = "";
+    var n = url.lastIndexOf("/");
+    if (n >= 0) {
+        var name = url.substring(n + 1, url.length - 4); // remove .git
+    }
+    return name;
+}
+
+function onRepositoryChanged() {
+    var textfield = document.getElementById("git_repository_text_field");
+    var url = textfield.value;
+    console.log(url);
+    if (url) {
+        var projectName = nameFromGitUrl(url);
+        console.log(projectName);
+        if (projectName) {
+            var projectNameText = document.getElementById("project_name_text_field");
+            projectNameText.value = projectName;
+        }
+    }
+}
+
+function generate_script() {
     var script = "";
 
-    var projectName = document.getElementById("project_name_text_field").value;
-    var projectFolder = document.getElementById("folder_name_text_field").value;
+    var projectName = document.getElementById("project_name_text_field").value.replace(/\s/g, '');
+    var projectUrl = document.getElementById("git_repository_text_field").value;
     var enabledLibs = getEnabledLibraries();
 
     script += "#!/bin/bash\n";
     script += "project_name=" + projectName + "\n";
-    script += "project_folder_name=" + projectFolder+ "\n";
     script += "source_folder_name=src\n";
-    script += "is_library=false\n";
+
     script += "\n";
     script += "submodule_urls=(\n"
-    for(const lib of enabledLibs)
-    {
+    for (const lib of enabledLibs) {
         script += lib.url + " \n";
     }
     script += ")\n";
     script += "submodule_names=(\n";
-    for(const lib of enabledLibs)
-    {
+    for (const lib of enabledLibs) {
         script += lib.name + " \n";
     }
     script += ")\n";
@@ -306,8 +286,7 @@ function generate_script()
     script += "    echo set\\(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE \\\${BIN_DIR}\\) >> $1\n";
     script += "    echo set\\(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO \\\${BIN_DIR}\\) >> $1\n";
     script += "    echo set\\(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG \\\${BIN_DIR}\\) >> $1\n";
-    if(contains(enabledLibs, "glow"))
-    {
+    if (contains(enabledLibs, "glow")) {
         script += "    echo set\\(GLOW_BIN_DIR \\\${CMAKE_SOURCE_DIR}/bin\\) >> $1\n";
         script += "    echo >> $1\n";
     }
@@ -342,8 +321,7 @@ function generate_script()
     script += "    echo >> $1\n";
     script += "\n";
 
-    if(contains(enabledLibs,"glfw"))
-    {
+    if (contains(enabledLibs, "glfw")) {
         script += "    echo \"# ===============================================\" >> $1\n";
         script += "    echo \\\# Mute some GLWF warnigns >> $1\n";
         script += "    echo option\\(GLFW_BUILD_EXAMPLES \\\"\\\" OFF\\) >> $1\n";
@@ -353,7 +331,7 @@ function generate_script()
         script += "    echo >> $1\n";
         script += "\n";
     }
-    
+
     script += "    echo \"# ===============================================\" >> $1\n";
     script += "    echo >> $1 \\\# Set link libraries\n";
     script += "    echo target_link_libraries\\(\\${PROJECT_NAME} PUBLIC >> $1\n";
@@ -379,26 +357,57 @@ function generate_script()
     script += "    echo \"    \" \\) >> $1\n";
     script += "    echo endif\\(\\) >> $1\n";
     script += "\n";
-    script += "    # todo: add test folder and executable\n";
-    script += "\n";
     script += "}\n";
     script += "\n";
-    script += "mkdir \"$project_folder_name\"\n";
-    script += "cd \"$project_folder_name\"\n";
+
+    // check if directory already exists
+    script += "if [ -d \"$project_name\" ]; then\n";
+    script += "    echo Directory " + projectName + " already exists!\n";
+    script += "    return 1\n";
+    script += "fi\n";
+    script += "\n";
+
+    if (projectUrl) {
+        onProjectNameChanged
+        script += "git clone " + projectUrl + " " + projectName;
+        script += "cd \"$project_name\"\n";
+    }
+    else {
+        script += "echo \"No git repository given. Init new repository?\"\n";
+        script += "USER_INPUT=\"\"\n";
+        script += "while [ -z \"$USER_INPUT\" ] || \
+        { [ \"$USER_INPUT\" != \"YES\" ]\
+         && [ \"$USER_INPUT\" != \"Y\" ]\
+          && [ \"$USER_INPUT\" != \"NO\" ]\
+           && [ \"$USER_INPUT\" != \"N\" ] };\n";
+        script += "do\n";
+        script += "echo $USER_INPUT\n"; // debug
+        script += "    echo -n \"[yes/[no]]:\"\n";
+        script += "    read USER_INPUT\n";
+        script += "    if [ -z \"$USER_INPUT\" ]; then\n";
+        script += "        echo no\n";
+        script += "        USER_INPUT=NO\n";
+        script += "    fi\n";
+        script += "    USER_INPUT=$(echo $USER_INPUT | tr a-z A-Z)\n";
+        script += "done\n";
+
+        script += "if [[ $USER_INPUT == \"YES\" || $USER_INPUT == \"Y\" ]]; then\n";
+        script += "    mkdir \"$project_name\"\n";
+        script += "    cd \"$project_name\"\n";
+        script += "else\n";
+        script += "    echo Aborting project setup.\n";
+        script += "    return 1\n";
+        script += "fi\n";
+    }
+
     script += "\n";
     script += "make_cmakelists \"CMakeLists.txt\"\n";
-    script += "touch \"readme.md\"\n";
+    script += "touch \"README.MD\"\n";
     script += "mkdir \"extern\"\n";
     script += "mkdir \"src\"\n";
     script += "mkdir \"bin\"\n";
     script += "\n";
-    script += "if $is_library; then\n";
-    script += "    mkdir \"src/$project_folder_name\"\n";
-    script += "    mkdir \"test\"\n";
-    script += "    make_main \"test/main.cc\"\n";
-    script += "else\n";
     script += "    make_main \"src/main.cc\"\n";
-    script += "fi\n";
     script += "\n";
     script += "git init\n";
     script += "\n";
@@ -415,7 +424,6 @@ function generate_script()
     return script;
 }
 
-window.onload = function()
-{
+window.onload = function () {
     this.setupCheckboxes();
 }
