0434557d8b6a8b3c8a543492f0592dd8f3b9dc65
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2020-02-14 13:04:49 +0100

    Some simplicfications.

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ class Library(object):
         self.__dict__ = data
 
 
-def load_libraries(origin_url : str):
+def load_libraries(origin_url: str):
     with urllib.request.urlopen(origin_url + "libraries.json") as url:
         data = json.loads(url.read().decode())
         libraries = []
@@ -23,23 +23,29 @@ def load_libraries(origin_url : str):
 
 def create_main(filepath):
     with open(filepath, "w") as f:
-        f.write('#include <cstdio>\n\n')
-        f.write('int main()\n')
-        f.write('{\n')
-        f.write('    printf("application main\\n");\n')
-        f.write('    return 0;\n')
-        f.write('}\n')
+        f.write(
+"""
+# include <cstdio>
+
+int main()
+{
+    printf("application main\\n");
+    return 0;
+}
+""")
 
 
-def create_cmakelists(filepath, project_name : str, flags_linux : str, flags_msvc : str, enabled_libraries):
+def create_cmakelists(filepath, project_name: str, flags_linux: str, flags_msvc: str, enabled_libraries):
     with open(filepath, "w") as f:
-        
+
         project_caps_prefix = project_name[0:3].upper()
 
         # HEADER
-        f.write('cmake_minimum_required(VERSION 3.8)\n')
-        f.write('project(' + project_name + ')\n\n')
-        f.write("""
+        f.write(
+"""
+cmake_minimum_required(VERSION 3.8)\n')
+project({project_name})
+
 # ===============================================
 # global settings
 
@@ -50,19 +56,19 @@ set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 # ===============================================
 # options
 
-option({0}_ENABLE_ASAN "if true, enables clang/MSVC address sanitizer" OFF)
-option({0}_ENABLE_MSAN "if true, enables clang/MSVC memory sanitizer" OFF)
-option({0}_ENABLE_UBSAN "if true, enables clang/MSVC undefined behaviour sanitizer" OFF)
-option({0}_ENABLE_TSAN "if true, enables clang/MSVC thread sanitizer" OFF)
+option({project_caps_prefix}_ENABLE_ASAN "if true, enables clang/MSVC address sanitizer" OFF)
+option({project_caps_prefix}_ENABLE_MSAN "if true, enables clang/MSVC memory sanitizer" OFF)
+option({project_caps_prefix}_ENABLE_UBSAN "if true, enables clang/MSVC undefined behaviour sanitizer" OFF)
+option({project_caps_prefix}_ENABLE_TSAN "if true, enables clang/MSVC thread sanitizer" OFF)
 
-if ({0}_ENABLE_ASAN AND {0}_ENABLE_TSAN)
+if ({project_caps_prefix}_ENABLE_ASAN AND {project_caps_prefix}_ENABLE_TSAN)
     message(FATAL_ERROR "Can only enable one of TSan or ASan at a time")
 endif()
-if ({0}_ENABLE_ASAN AND {0}_ENABLE_MSAN)
+if ({project_caps_prefix}_ENABLE_ASAN AND {project_caps_prefix}_ENABLE_MSAN)
     message(FATAL_ERROR "Can only enable one of ASan or MSan at a time")
 endif()
 
-option({0}_ENABLE_WERROR "if true, enables -Werror, /WX" OFF)
+option({project_caps_prefix}_ENABLE_WERROR "if true, enables -Werror, /WX" OFF)
 
 
 # ===============================================
@@ -72,39 +78,39 @@ set(COMMON_COMPILER_FLAGS "")
 set(COMMON_LINKER_FLAGS "")
 
 if (MSVC)
-    list(APPEND COMMON_COMPILER_FLAGS {2})
+    list(APPEND COMMON_COMPILER_FLAGS {flags_msvc})
 
-    if ({0}_ENABLE_WERROR)
+    if ({project_caps_prefix}_ENABLE_WERROR)
         list(APPEND COMMON_COMPILER_FLAGS /WX)
     endif()
 else()
-    list(APPEND COMMON_COMPILER_FLAGS {1})
+    list(APPEND COMMON_COMPILER_FLAGS {flags_linux})
 
-    if ({0}_ENABLE_WERROR)
+    if ({project_caps_prefix}_ENABLE_WERROR)
         list(APPEND COMMON_COMPILER_FLAGS -Werror)
     endif()
 
-    if ({0}_ENABLE_ASAN OR {0}_ENABLE_TSAN OR {0}_ENABLE_MSAN OR {0}_ENABLE_UBSAN)
+    if ({project_caps_prefix}_ENABLE_ASAN OR {project_caps_prefix}_ENABLE_TSAN OR {project_caps_prefix}_ENABLE_MSAN OR {project_caps_prefix}_ENABLE_UBSAN)
         list(APPEND COMMON_COMPILER_FLAGS -fno-omit-frame-pointer -g)
         list(APPEND COMMON_LINKER_FLAGS -fno-omit-frame-pointer -g)
     endif()
 
-    if ({0}_ENABLE_ASAN)
+    if ({project_caps_prefix}_ENABLE_ASAN)
         list(APPEND COMMON_COMPILER_FLAGS -fsanitize=address)
         list(APPEND COMMON_LINKER_FLAGS -fsanitize=address)
     endif()
 
-    if ({0}_ENABLE_TSAN)
+    if ({project_caps_prefix}_ENABLE_TSAN)
         list(APPEND COMMON_COMPILER_FLAGS -fsanitize=thread)
         list(APPEND COMMON_LINKER_FLAGS -fsanitize=thread)
     endif()
 
-    if ({0}_ENABLE_MSAN)
+    if ({project_caps_prefix}_ENABLE_MSAN)
         list(APPEND COMMON_COMPILER_FLAGS -fsanitize=memory)
         list(APPEND COMMON_LINKER_FLAGS -fsanitize=memory)
     endif()
 
-    if ({0}_ENABLE_UBSAN)
+    if ({project_caps_prefix}_ENABLE_UBSAN)
         list(APPEND COMMON_COMPILER_FLAGS
             -fsanitize=undefined
             -fno-sanitize-recover=all
@@ -118,37 +124,39 @@ else()
     endif()
 endif()
 
-        """.format(project_caps_prefix, flags_linux, flags_msvc))
-        f.write('\n')
-        f.write('# ===============================================\n')
-        f.write('# Bin dir\n')
-        f.write('if(MSVC)\n')
-        f.write('    set(BIN_DIR ${CMAKE_SOURCE_DIR}/bin)\n')
-        f.write('elseif(CMAKE_BUILD_TYPE STREQUAL "")\n')
-        f.write('    set(BIN_DIR ${CMAKE_SOURCE_DIR}/bin/Default)\n')
-        f.write('else()\n')
-        f.write(
-            '    set(BIN_DIR ${CMAKE_SOURCE_DIR}/bin/${CMAKE_BUILD_TYPE})\n')
-        f.write('endif()\n')
-        f.write('set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${BIN_DIR})\n')
-        f.write('set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${BIN_DIR})\n')
-        f.write(
-            'set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${BIN_DIR})\n')
-        f.write('set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${BIN_DIR})\n')
+# ===============================================
+# Bin dir
+if(MSVC)
+    set(BIN_DIR ${{CMAKE_SOURCE_DIR}}/bin)
+elseif(CMAKE_BUILD_TYPE STREQUAL "")
+    set(BIN_DIR ${{CMAKE_SOURCE_DIR}}/bin/Default)
+else()
+'    set(BIN_DIR ${{CMAKE_SOURCE_DIR}}/bin/${{CMAKE_BUILD_TYPE}})
+endif()
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${{BIN_DIR}})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${{BIN_DIR}})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${{BIN_DIR}})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${{BIN_DIR}})
+""".format(
+    project_name=project_name,
+    project_caps_prefix=project_caps_prefix,
+    flags_msvc=flags_msvc,
+    flags_linux=flags_linux))
 
         # GLOW needs a special bin directory
         if any(lib.name == "glow" for lib in enabled_libraries):
-            f.write("set(GLOW_BIN_DIR ${CMAKE_SOURCE_DIR}/bin)\n")
+            f.write("set(GLOW_BIN_DIR ${{CMAKE_SOURCE_DIR}}/bin)\n")
 
         # GLFW is annoying
         if(any(lib.name == "glfw" for lib in enabled_libraries)):
-            f.write("\n")
-            f.write('# ===============================================\n')
-            f.write('# disable glfw additionals\n')
-            f.write('option(GLFW_BUILD_EXAMPLES "" OFF)\n')
-            f.write('option(GLFW_BUILD_TESTS "" OFF)\n')
-            f.write('option(GLFW_BUILD_DOCS "" OFF)\n')
-            f.write('option(GLFW_INSTALL "" OFF)\n')
+            f.write(
+"""# ===============================================
+# disable glfw additionals
+option(GLFW_BUILD_EXAMPLES "" OFF)
+option(GLFW_BUILD_TESTS "" OFF)
+option(GLFW_BUILD_DOCS "" OFF)
+option(GLFW_INSTALL "" OFF)
+""")
 
         # Add the submodules
         f.write("\n")
@@ -183,19 +191,20 @@ endif()
         f.write("    ${COMMON_LINKER_FLAGS}\n")
         f.write(')\n')
         f.write('target_include_directories(${PROJECT_NAME} PUBLIC "src")\n')
-        f.write('target_compile_options(${PROJECT_NAME} PUBLIC ${COMMON_COMPILER_FLAGS})')
+        f.write(
+            'target_compile_options(${PROJECT_NAME} PUBLIC ${COMMON_COMPILER_FLAGS})')
 
 
 def get_enabled_libs(args):
-    all_libs = load_libraries(args.origin)
+    all_libs=load_libraries(args.origin)
     return list(filter(lambda lib: lib.name in args.libraries, all_libs))
 
 
 def setup_project(args):
-    project_name = args.name
-    project_url = args.url
-    enabled_libs = get_enabled_libs(args)
-    origin_url = args.origin
+    project_name=args.name
+    project_url=args.url
+    enabled_libs=get_enabled_libs(args)
+    origin_url=args.origin
 
     if(os.path.isdir(project_name)):
         print("Directory " + project_name + " already exists! Aborting!")
@@ -214,15 +223,18 @@ def setup_project(args):
     Path(os.path.join(project_name, "README.md")).touch()
 
     # Download files
-    raw_data_download_url = origin_url
+    raw_data_download_url=origin_url
     if not "localhost" in origin_url:
         # extract github raw domain
-        github_username = origin_url.split("/")[2].split(".")[0]
-        github_reponame = origin_url.split("/")[3]
-        raw_data_download_url = "https://raw.githubusercontent.com/{}/{}/master".format(github_username, github_reponame)
+        github_username=origin_url.split("/")[2].split(".")[0]
+        github_reponame=origin_url.split("/")[3]
+        raw_data_download_url="https://raw.githubusercontent.com/{}/{}/master".format(
+            github_username, github_reponame)
 
-    urllib.request.urlretrieve(raw_data_download_url + "/data/.clang-format", os.path.join(project_name, ".clang-format"))
-    urllib.request.urlretrieve(raw_data_download_url + "/data/.gitignore", os.path.join(project_name, ".gitignore"))
+    urllib.request.urlretrieve(
+        raw_data_download_url + "/data/.clang-format", os.path.join(project_name, ".clang-format"))
+    urllib.request.urlretrieve(
+        raw_data_download_url + "/data/.gitignore", os.path.join(project_name, ".gitignore"))
 
 
     os.mkdir(os.path.join(project_name, "extern"))
@@ -232,11 +244,13 @@ def setup_project(args):
     create_main(os.path.join(project_name, 'src', 'main.cc'))
 
     for lib in enabled_libs:
-        extern_folder = os.path.join(project_name, "extern")
+        extern_folder=os.path.join(project_name, "extern")
         # try ssh first, https second
-        success = call(["git", "-C", extern_folder , "submodule", "add", lib.git_url_ssh, lib.name])
+        success=call(["git", "-C", extern_folder, "submodule",
+                     "add", lib.git_url_ssh, lib.name])
         if success != 0:
-            success = call(["git", "-C", extern_folder , "submodule", "add", lib.git_url_https, lib.name])
+            success=call(["git", "-C", extern_folder, "submodule",
+                         "add", lib.git_url_https, lib.name])
         if success != 0:
             print("Failed to clone " + lib.name + "!", file=sys.stderr)
 
@@ -247,7 +261,7 @@ def setup_project(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    parser=argparse.ArgumentParser(
         description="Create project with dependencies")
     parser.add_argument("--name", "-n", type=str, required=True)
     parser.add_argument("--origin", "-o", type=str, required=True)
@@ -255,6 +269,6 @@ if __name__ == "__main__":
     parser.add_argument("--flags_msvc", "-m", type=str)
     parser.add_argument("--url", "-u", type=str)
     parser.add_argument("libraries", nargs='*', type=str)
-    args = parser.parse_args()
+    args=parser.parse_args()
 
     setup_project(args)
